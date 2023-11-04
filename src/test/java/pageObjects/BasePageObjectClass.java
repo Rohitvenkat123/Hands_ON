988f21668592854a1fbeced4dbe4f49855f55ccd
Rohitvenkat123 <venkata-subbaiah.singaresi@capgemini.com>
2023-11-04 23:15:35 +0530

    First Commit

---
+++ src/test/java/pageObjects/BasePageObjectClass.java
@@ -0,0 +1,25 @@
+package pageObjects;
+
+
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePageObjectClass {
+
+	 WebDriver driver;
+	    
+	   public BasePageObjectClass(WebDriver driver)
+	     {
+		     this.driver=driver;
+		     PageFactory.initElements(driver,this);
+	     }
+	   	   
+}
+
+
+
+
+
